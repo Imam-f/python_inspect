421819773a88869bc4add62a5c8013af648fc868
Imam-F <mohamadimamfirdaus17@gmail.com>
2024-11-26 21:23:00 +0700

    mod pickle print

--- pickle_print.py
+++ pickle_print.py
@@ -4,6 +4,9 @@ import sys
 import pickle
 import pprint
 
+from inspect_instance import inspect_object, A
+from inspect_inheritance import get_class_details
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: python inspect_pickle.py <pickle_file>")
@@ -14,7 +17,10 @@ def main():
     try:
         with open(pickle_file, 'rb') as f:
             data = pickle.load(f)
-        pprint.pprint(data)
+        # pprint.pprint(data)
+        
+        get_class_details(data.__class__)
+        inspect_object(data)
     except Exception as e:
         print(f"Error loading pickle file: {e}")
         sys.exit(1)
