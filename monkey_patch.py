e8f204d914dc7ddbf7654d3fc6ee8e9a940e2ed3
Imam-f <mohamadimamfirdaus17@gmail.com>
2024-12-14 13:40:02 +0700

    cursed monkey patching

---
+++ monkey_patch.py
@@ -0,0 +1,6 @@
+from forbiddenfruit import curse
+def words_of_wisdom(self):
+    return self * "blah "
+curse(int, "words_of_wisdom", words_of_wisdom)
+assert (2).words_of_wisdom() == "blah blah "
+print((2).words_of_wisdom())
